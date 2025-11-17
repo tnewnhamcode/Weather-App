1cf0847d0c0d9145ae689aa40a9dd9881481ba26
tnewnhamcode <148156667+tnewnhamcode@users.noreply.github.com>
2025-11-17 13:56:56 +0000

    Update urls.py

--- project2og/urls.py
+++ project2og/urls.py
@@ -21,6 +21,7 @@ from django.urls import include
 from weather.views import MyModelView
 from rest_framework import routers
 import weather
+import settings
 
 
 router = routers.DefaultRouter()    #Creates a router instance. The `DefaultRouter`
@@ -44,4 +45,5 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
