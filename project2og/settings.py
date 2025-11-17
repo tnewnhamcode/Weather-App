93e0109bdeebf94259f96a6b6b50bf38a5e7a8fd
tnewnhamcode <148156667+tnewnhamcode@users.noreply.github.com>
2025-11-17 13:47:47 +0000

    Update settings.py

--- project2og/settings.py
+++ project2og/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent    #its a path object imported from t-path library sense
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# Media files (user-uploaded)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
