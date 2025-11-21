72bf6fc083731a9a370045f8f987fd50a11f2ae3
tnewnhamcode <148156667+tnewnhamcode@users.noreply.github.com>
2025-11-21 11:33:51 +0000

    Update settings.py

--- project2og/settings.py
+++ project2og/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent    #its a path object imported
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eu&%03=8n+0mqvo*qduxe@#nl7zeyl*louert2$4hcw)$%fm(#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
